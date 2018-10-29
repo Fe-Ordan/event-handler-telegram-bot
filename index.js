8cd49512f5eccb52a3a9cb4336ce8d63b9e1140d
fatihint <fatihint@gmail.com>
2018-10-29 11:48:28 +0300

    Persistence added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
+const Datastore = require('nedb')
+
+const db = new Datastore({ filename: 'store.db', autoload: true })
 
 const { TELEGRAM_BOT_API_TOKEN } = require('./config.json')
 
@@ -8,10 +11,28 @@ bot.on('message', (msg) => {
 
     if (msg.text.match(/\/start/)) {
         bot.sendMessage(msg.chat.id, `Let's start, send me the meeting title !`)
-    }
-    
-    if (msg.text.match(/\/finish/)) {
+
+        db.findOne({ _id: msg.chat.id }, (err, doc) => {
+            if (doc) {
+                console.log('doc', doc)
+            } else {
+                doc = {
+                    _id: msg.chat.id,
+                    active: true
+                }
+
+                db.insert(doc, (err, newDoc) => {
+                    console.log("newDoc: ", newDoc)
+                    console.log("err", err)
+                })
+            }
+        })
+    } else if (msg.text.match(/\/finish/)) {
         bot.sendMessage(msg.chat.id, `Finish`)
+    } else {
+        db.update({ _id: msg.chat.id }, {$set: {title: msg.text}}, {returnUpdatedDocs: true}, (err, numAffected, affectedDoc) => {
+            console.log("affectedDoc: ", affectedDoc)
+            console.log("err: ", err)
+        })
     }
-    
 })
