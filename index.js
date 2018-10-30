d75b29f88b8426ee26d85fafdfced552c70ce6a1
fatihint <fatihint@gmail.com>
2018-10-30 16:27:58 +0300

    Share created event in group.

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const request = require('request')
 
 const db = new Datastore({ filename: 'store.db', autoload: true })
 
-const { TELEGRAM_BOT_API_TOKEN } = require('./config.json')
-const { GOOGLE_API_TOKEN } = require('./config.json')
+const { TELEGRAM_BOT_API_TOKEN, GOOGLE_API_TOKEN, JWT_SECRET } = require('./config.json')
 
 const bot = new TelegramBot(TELEGRAM_BOT_API_TOKEN, { polling: true })
 
@@ -14,30 +13,42 @@ const bot = new TelegramBot(TELEGRAM_BOT_API_TOKEN, { polling: true })
  */
 bot.on('message', (msg) => {
 
-    console.log(JSON.stringify(msg, 0, 2))
-
     if (msg.text.match(/\/start/)) {
-        bot.sendMessage(msg.chat.id, `Let's start, send me the meeting title !`)
+        var msgElements = msg.text.split(' ')
+        var event = {}
 
-        db.remove({ _chatId: msg.chat.id, active: true }, { multi: true }, (err, numRemoved) => {
-            doc = {
+        /** If hashed message info exists,
+         * update the event's chatId and start getting answers.
+        */
+        if (msgElements[1]) {
+            var token = msgElements[1]
+            db.remove({ _chatId: msg.chat.id, active: true }, { multi: true }, () => {
+                db.update({ _id: token }, { $set: { _chatId: msg.chat.id } }, { returnUpdatedDocs: true }, (err, numAffected, affectedDoc) => {
+                    if (affectedDoc) {
+                        generateEvent(affectedDoc, msg)
+                    }
+                })
+            })
+        } else {
+            bot.sendMessage(msg.chat.id, `Let's start, send me the meeting title !`)
+            event = {
                 _chatId: msg.chat.id,
                 active: true,
                 readyToPublished: false
             }
-
-            db.insert(doc, (err, newDoc) => {
-                console.log("newDoc: ", newDoc)
-                console.log("err", err)
+            db.remove({ _chatId: msg.chat.id, active: true }, { multi: true }, () => {
+                db.insert(event, (err, newDoc) => {
+                    if (err) throw err
+                })
             })
-        })
-    } else if(msg.text.match(/\/events/)) {
-        db.findOne({_chatId: msg.chat.id, active: true, readyToPublished: true}, (err, doc) => {
+        }
+    } else if (msg.text.match(/\/events/)) {
+        db.findOne({ _chatId: msg.chat.id, active: true, readyToPublished: true }, (err, doc) => {
             if (doc) {
                 generateEvent(doc, msg)
             } else {
                 bot.sendMessage(msg.chat.id, `Can't find any active events. Send /start to create a new one.`)
-            }   
+            }
         })
     }
     else {
@@ -47,7 +58,7 @@ bot.on('message', (msg) => {
                     bot.sendMessage(msg.chat.id, `Great,now send me the *date* or *time* for ${msg.text} meeting.`, { parse_mode: "Markdown" })
                     db.update({ _chatId: msg.chat.id, active: true }, { $set: { title: msg.text } }, { returnUpdatedDocs: true })
                 } else if (!doc.date) {
-                    bot.sendMessage(msg.chat.id, `Send me the *location* for the meeting.`, { parse_mode: "Markdown"})
+                    bot.sendMessage(msg.chat.id, `Send me the *location* for the meeting.`, { parse_mode: "Markdown" })
                     db.update({ _chatId: msg.chat.id, active: true }, { $set: { date: msg.text } }, { returnUpdatedDocs: true })
                 } else if (!doc.location) {
                     bot.sendMessage(msg.chat.id, `Event ready !`)
@@ -78,10 +89,10 @@ bot.on('message', (msg) => {
  */
 function generateEvent(doc, msg) {
     var message = ''
-    
+
     if (msg.chat.type === 'private') {
         message += 'Event created. Use this link to share it to a group:\n'
-        message += 'http://t.me/meetingsetterbot?startgroup=qweasd  \n\n'
+        message += `http://t.me/meetingsetterbot?startgroup=${doc._id}\n\n`
     }
 
     message += `*${doc.title}* \n\n \uD83D\uDCC5  ${doc.date} \n\n Adress: ${doc.location.address}`
